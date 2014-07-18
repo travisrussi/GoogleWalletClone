6a621d0202474cda7cd61c0acefb3e0721fbd2bd
tricinel <bogdan.freelance@gmail.com>
2014-07-18 11:59:25 +0200

    finished popup-2 and waiting-1

--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -1,6 +1,30 @@
 'use strict';
 angular.module('Gwallet')
 
-  .controller('AppCtrl', ['$scope', function($scope) {
-    console.log($scope);
+  .controller('AppCtrl', ['$scope', '$ionicPopup', '$ionicLoading', function($scope, $ionicPopup, $ionicLoading) {
+    // Show popup code
+    $scope.showPopup = function(id) {
+      $ionicPopup.confirm({
+        title: 'Terms of Service',
+        templateUrl: 'templates/modals/popup-' + id + '.html',
+        buttons: [
+          {
+            text: 'Cancel',
+            type: 'button-gray'
+          }, {
+            text: 'Accept',
+            type: 'button-positive'
+          }
+        ]
+      });
+    };
+
+    // Show loading overlay
+    $scope.showLoading = function() {
+      $ionicLoading.show({
+        templateUrl: 'templates/modals/waiting-1.html',
+        duration: 3000
+      });
+    };
+
   }]);
