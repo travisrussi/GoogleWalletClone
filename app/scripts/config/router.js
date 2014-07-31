3e3cc4217a6f122499335cd2752e2d13474a90b4
tricinel <bogdan.freelance@gmail.com>
2014-07-31 11:58:51 +0200

    some cleanup first

--- app/scripts/config/router.js
+++ app/scripts/config/router.js
@@ -155,44 +155,7 @@ angular.module('Gwallet')
           templateUrl: 'templates/cards/cards-10.html'
         }
       }
-    })
-
-    .state('app.search', {
-      url: '/search',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/search.html'
-        }
-      }
-    })
-
-    .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: '/playlists/:playlistId',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/playlist.html',
-          controller: 'PlaylistCtrl'
-        }
-      }
     });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/full-page-2');
 }]);
--- app/scripts/controllers/playlist.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-angular.module('Gwallet')
-
-  .controller('PlaylistCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
-    console.log($scope, $stateParams);
-  }]);
